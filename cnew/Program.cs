f89f21045572becdad79fe6515ae684497b61810
Hidayet Eren Uludağ <h.eren.uludag@gmail.com>
2024-06-17 15:48:05 +0300

    feat: Refactor Program.cs to improve class name handling

--- cnew/Program.cs
+++ cnew/Program.cs
@@ -3,6 +3,9 @@ using Hellow;
 using System.IO;
 using QuickGraph;
 using Microsoft.VisualBasic;
+using QuickGraph.Graphviz;
+using IDotEngine;
+
 namespace Hello
 {
     class CsvParser
@@ -23,25 +26,49 @@ namespace Hello
                     listA.Add(values[0]);
                     listB.Add(fullname);
 
-                    List<string> classes = new List<string>(values[3].Split(',').Select(s => s.Trim()));
+                    List<string> classes = new List<string>();
+                    for (int i = 3; i < values.Length; i++) {
+                        classes.Add(values[i].Trim());
+                    }
                     listC.Add(classes);
                 }
             }
-            
             AdjacencyGraph<string, Edge<string>> myGraph = new AdjacencyGraph<string, Edge<string>>();
             for(int i = 0; i<listB.Count; i++){
-                for(int j = 0; j<listC[i].Count; j++){
-                    Console.WriteLine(listB[i]);
-                    Console.WriteLine(listC[i][j]);
+                //Console.WriteLine($"Student: {listB[i]} takes the following classes:");
+                foreach(var className in listC[i]){
+                    //Console.WriteLine(className);
+                    if(className[0]=='\"'){
+                        string x = className.Remove(0,1);
+                        // Console.WriteLine(x);
+                    }
+                    
+                    if(className[className.Length-1]=='\"'){
+                        string x = className.Remove(className.Length-1);
+                        //Console.WriteLine(x);
+                    }
+                    myGraph.AddVertex(className);
+                    
+                }
+                for(int j = 0; j<listC[i].Count;j++ ){
+                    for(int k = j+1; k<listC[i].Count; k++){
+                        var edgemy = new Edge<string>(listC[i][j],listC[i][k]);
+                    }
                 }
             }
-
-
         }
     }
-    class Grapher
+    public class FileDotEngine : IDotEngine
+{    
+    public string Run(GraphvizImageType imageType, string dot, string outputFileName)
     {
-        
+        using (StreamWriter writer = new StreamWriter(outputFileName))
+        {
+            writer.Write(dot);    
+        }
+
+        return System.IO.Path.GetFileName(outputFileName);
     }
-    }
+}
+}
 
